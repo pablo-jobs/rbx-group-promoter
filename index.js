919b32f1150270fb4a0aca10e14b0e8f1809a593
Tochi Ukegbu <tochiukegbu@gmail.com>
2022-07-11 15:47:07 -0400

    logging + converted content split to regex

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import { createServer } from "http";
 const { post } = axios;
 const { setCookie, getGroup, getRole, setRank } = noblox;
 
-const MAX_MESSAGE_CONTENT_LENGTH = 1900;
-
 process.on("uncaughtException", console.error);
 
 try {
@@ -25,39 +23,11 @@ const { name: groupName } = await getGroup(GROUP_ID);
 const promoteRole = await getRole(GROUP_ID, parseInt(process.env.ROLE_RANK));
 
 async function sendToWebhook(content) {
-    if (content.length <= MAX_MESSAGE_CONTENT_LENGTH) {
-        return post(process.env.WEBHOOK_URL, { content });
-    } else {
-        let payload = [];
-        let payloadCharLength = 0;
-        let lines = content.split("\n");
-        let request = Promise.resolve();
-        
-        let i = 0;
-        while (i < lines.length) {
-            const line = lines[i];
-            if (payloadCharLength + payload.length + line.length > MAX_MESSAGE_CONTENT_LENGTH) {
-                if (payload.length) {
-                    const c = payload.join("\n");
-                    request = request.then(() => post(process.env.WEBHOOK_URL, { content: c }));
-                    payload = [];
-                    payloadCharLength = 0;
-                } else {
-                    lines.splice(i, 1, line.substring(0, MAX_MESSAGE_CONTENT_LENGTH), line.substring(MAX_MESSAGE_CONTENT_LENGTH, line.length));
-                }
-            } else {
-                payload.push(line);
-                payloadCharLength += line.length;
-                i++;
-            }
-        }
-
-        if (payload.length) {
-            request = request.then(() => post(process.env.WEBHOOK_URL, { content: payload.join("\n") }));
-        }
-
-        return request;
-    }
+    // 1900 is content length max (2000 is but 100 char buffer)
+    return content.match(/[\s\S]{1,1900}(?![^\n])/gm).map(s => s.trim()).reduce(
+        (promise, content) => promise.then(() => post(process.env.WEBHOOK_URL, { content })), 
+        Promise.resolve()
+    );
 }
 
 async function set(userId, userName) {
@@ -93,13 +63,20 @@ createServer(function (req, res) {
         if (body.pass === process.env.PASSWORD) {
             let userId = parseInt(body.userId);
             if (!isNaN(userId) && userId > 0 && body.username && body.report) {
+                console.log(`Processing @${body.username} (${userId}) for application ${body.passed ? "success" : "failure"}`);
                 try {
-                    await sendToWebhook(body.report).finally(() => body.passed && set(userId, body.username));
-                    res.writeHead(200);
+                    await sendToWebhook(body.report);
                 } catch (e) {
                     console.error(e);
-                    res.writeHead(500);
                 }
+                if (body.passed) {
+                    try {
+                        await set(userId, body.username);
+                    } catch (e) {
+                        console.error(e);
+                    }
+                }
+                res.writeHead(200);
             } else {
                 res.writeHead(400);
             }
